5a34751bc150a622a6c2245319a8adcad1cb003b
Balrajsingh2044 <balrajsingh.2044@gmail.com>
2025-08-01 14:50:20 +0530

    Lecture 8

--- lecture08/reverse.cpp
+++ lecture08/reverse.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int swap(int x, int y){
+int swap(int &x, int &y){
         int temp;
         temp = x;
         x = y;
@@ -11,18 +11,20 @@ int swap(int x, int y){
 
 
 int main() {
-    int arr[] = {1,2,3,4,5,6,7,8,9,0};
+    int arr[] = {1,2,3,4,5,6,7,8,9};
 
     cout << "Original" << endl;
+    int size = 0;
 
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < 9; i++){
         cout << arr[i] << " ";
+        size++;
     }
 
     cout << endl;
 
     int start = 0; 
-    int end = -1;
+    int end = size -1;
 
     while (start <= end){
         swap(arr[start], arr[end]);
@@ -32,7 +34,7 @@ int main() {
 
     cout << "Reverse" << endl;
 
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < size; i++){
         cout << arr[i] << " ";
     }
 
