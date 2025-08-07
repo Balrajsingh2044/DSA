1a2136efb0ba2874e84345f5a2e39c73c38f7ff7
Balrajsingh2044 <balrajsingh.2044@gmail.com>
2025-08-08 01:00:20 +0530

    Uploaading

--- lecture17/bs.cpp
+++ lecture17/bs.cpp
@@ -25,10 +25,28 @@ int binarysearch(const vector<int>& nums, int target){
 
 }
 
+int recbinarySearch(vector<int> & nums, int target, int st, int end){
+    if(st <= end){
+        if(target < nums[(st+end)/2]){
+            return recbinarySearch(nums, target, st, ((st+end)/2)-1);
+        }
+
+        else if (target > nums[(st + end)/2]){
+            return recbinarySearch(nums, target, ((st+end)/2)+1, end);
+        }
+        else{
+            return (st+end)/2;
+        }
+
+    }
+    return -1;
+}
+
 int main() {
     vector<int> nums = {-1,0,3,4,5,9,12};
-    int target = 9;
+    int target = 0;
 
 
-    cout << binarysearch(nums, target) << endl;
+
+    cout << recbinarySearch(nums, target, 0, nums.size()-1) << endl;
 }
